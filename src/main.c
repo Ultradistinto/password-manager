3a1fdbdaf2f537132796d02ed6180b691ed54978
Ultra <larro200410@gmail.com>
2025-07-19 05:09:18 -0300

    Prefiero hacerlo en C

---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>   // This is C
+
+int main(){
+    printf("Prueba de Build en C");
+    return 0;
+}
--- src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main(){
-    std:: cout << "Prueba de Build";
-    return 0;
-}
